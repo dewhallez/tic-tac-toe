8820e0003c3cf8770017b0a721851881ddb76736
zxy12345 <tamiquedebrito@gmail.com>
2017-02-23 15:17:06 -0800

    Update Algorithm.py

--- Algorithm.py
+++ Algorithm.py
@@ -3,15 +3,16 @@ def Victory(Board):
     
     for i in range(3):
         if Board[i][1] == Board[i][2] == Board[i][3]:
-            Win = Board[i][1]
+            return Board[i][1]
 
     for i in range(3):
         if Board[1][i] == Board[2][i] == Board[3][i]:
-            Win = Board[1][i]
+            return Board[1][i]
     
     if Board[1][1] == Board[2][2] == Board[3][3]:
-            Win = Board[1][1]
+            return Board[1][1]
                 
     if Board[1][3] == Board[2][2] == Board[3][1]:
-            Win = Board[1][3]
-    return Win        
+            return Board[1][3]
+        
+    return ' '   
