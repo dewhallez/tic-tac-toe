6bd4c21c2d72b41f7815480d1c6c3982edff20b2
zxy12345 <tamiquedebrito@gmail.com>
2017-02-23 15:27:56 -0800

    Update Algorithm.py

--- Algorithm.py
+++ Algorithm.py
@@ -2,17 +2,17 @@ def Victory(Board):
     Win = ' '
     
     for i in range(3):
-        if Board[i][1] == Board[i][2] == Board[i][3]:
-            return Board[i][1]
+        if (Board[i][0] == Board[i][1] == Board[i][2]):
+            return Board[i][0]
 
     for i in range(3):
-        if Board[1][i] == Board[2][i] == Board[3][i]:
-            return Board[1][i]
+        if (Board[0][i] == Board[1][i] == Board[2][i]):
+            return Board[0][i]
     
-    if Board[1][1] == Board[2][2] == Board[3][3]:
-            return Board[1][1]
+    if (Board[0][0] == Board[1][1] == Board[2][2]):
+            return Board[0][0]
                 
-    if Board[1][3] == Board[2][2] == Board[3][1]:
-            return Board[1][3]
+    if (Board[0][2] == Board[1][1] == Board[2][0]):
+            return Board[0][2]
         
-    return ' '   
+    return ' '  
