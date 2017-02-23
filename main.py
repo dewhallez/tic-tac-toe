4b76546987f6f38ec393e44714c66ab0a11a30ab
Eliot Winchell <eliot.winchell@gmail.com>
2017-02-23 15:32:53 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+# Main Tic Tac Toe file
+#!/usr/bin/env python3
+
+# module imports
+import time
+
+# Player Dictionaries 
+playerOne = {'name': '', piece: '', 'timesWon': 0}
+playerTwo = {'name': '', piece: '', 'timesWon': 0}
+
+# 2D Array Game Board
+width, height = 3
+for x in range(width):
+    for y in range(height):
+        board[x][y] = ' '
+
+
+
+
+
+
+
