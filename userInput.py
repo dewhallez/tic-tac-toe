215041456629a628ac1a87e7c8843c370b7bbf8f
Eliot Winchell <eliot.winchell@gmail.com>
2017-02-27 23:09:02 -0800

    Update userInput.py

--- userInput.py
+++ userInput.py
@@ -1,29 +1,27 @@
-def startGame(playerOne,playerTwo):
-  print('Welcome to tic-tac-toe!')
+def startGame(playerOne, playerTwo):
+    print('Welcome to tic-tac-toe!')
 
-  p1name = str(input('Player 1 name?: '))
-  playerOne['name'] = p1name
+    p1name = input('Player 1 name?: ')
+    playerOne['name'] = p1name
 
-  p1piece = str(input('X or O?: '))
+    p1piece = input('X or O?: ').upper()
 
-  while p1piece not in ['x','X','o','O']:
-      print ('Invalid choice.')
-      p1piece = str(input('X or O?: '))
+    while p1piece not in ['X', 'O']:
+        print ('Invalid choice.')
+        p1piece = input('X or O?: ')
 
-  if p1piece in ['x','X']:
-       p1piece = 'X'
-       p2piece = 'O'
-  else:
-       p1piece = 'O'
-       p2piece = 'X'
+    if p1piece in ['X']:
+        p1piece = 'X'
+        p2piece = 'O'
+    else:
+        p1piece = 'O'
+        p2piece = 'X'
 
-  p2name = str(input('Player 2 name?'))
-  playerTwo['name'] = p2name
+    p2name = input('Player 2 name?: ')
+    playerTwo['name'] = p2name
 
-  playerOne['piece'] = p1piece
-  playerTwo['piece'] = p2piece
+    playerOne['piece'] = p1piece
+    playerTwo['piece'] = p2piece
 
-  print('Player 1: ',playerOne['name'],', is playing as ',playerOne['piece'])
-  print('Player 2: ',playerTwo['name'],', is playing as ',playerTwo['piece'])
-
-  return
+    print('Player 1: ' + playerOne['name'] + ', is playing as ' + playerOne['piece'])
+    print('Player 2: ' + playerTwo['name'] + ', is playing as ' + playerTwo['piece'])
