51ad95b0ed5430dcfd7830fd223b62b760a090af
Eliot Winchell <eliot.winchell@gmail.com>
2017-02-27 23:01:12 -0800

    bug fixes
    added game board, converted tabs to spaces

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 # Main Tic Tac Toe file
 #!/usr/bin/env python3
 
-# module imports
+# Module imports
+import algorithm
+from startInput import startGame
 import time
 
-# Player Dictionaries 
-playerOne = {'name': '', piece: '', 'timesWon': 0}
-playerTwo = {'name': '', piece: '', 'timesWon': 0}
+# Player Dictionaries and Global Variables
+playerOne = {'name': '', 'piece': '', 'timesWon': 0}
+playerTwo = {'name': '', 'piece': '', 'timesWon': 0}
+board = []
+width = height = 3
 
-# 2D Array Game Board
-width, height = 3
+startGame(playerOne, playerTwo)
+
+# Initialize empty 2-Dimensional Array
 for x in range(width):
+    row = []
     for y in range(height):
-        board[x][y] = ' '
+        row.append('_')
+    board.append(row)
 
-
-
+# Game loop
+while True:
+    # Draw the game board
+    for x in range(width):
+        for y in range(height):
+            if (y < 2):
+                print(board[x][y], end='|')
+            else:
+                print(board[x][y], end='')
+        print("")
+    nextMove = input("Player " + playerOne['name'] + ": Next move?")
 
 
 
