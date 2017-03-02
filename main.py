b2cfdf98c81202abb8066c4ef06404157bece9a3
Eliot Winchell <eliotwinchell@Eliots-MacBook-Air.local>
2017-03-02 15:35:17 -0800

    added input validation and more code

--- main.py
+++ main.py
@@ -11,24 +11,58 @@ import time
 playerOne = {'name': '', 'piece': '', 'timesWon': 0}
 playerTwo = {'name': '', 'piece': '', 'timesWon': 0}
 board = []
-width = height = 3
+WIDTH = HEIGHT = 3
+turn = ''
+x = 0
+y = 0
 
+def setCoordinate(turn):
+    x = int(input('Player ' + turn['name'] + ': x coordinate?'))
+    while ((x < 0) or (x > HEIGHT)):
+        print('Out of range, this is a ' + str(WIDTH) + ' x ' + str(HEIGHT) + ' board.')
+        x = int(input('Player ' + turn['name'] + ': x coordinate?'))
+
+    y = int(input('Player ' + turn['name'] + ': y coordinate?'))
+    print(y)
+    while ((y < 0) or (y > HEIGHT)):
+        print('Out of range, this is a ' + str(WIDTH) + ' x ' + str(HEIGHT) + ' board.')
+        y = int(input('Player ' + turn['name'] + ': y coordinate?'))
+
+# This function gets the user input before starting the game
 startGame(playerOne, playerTwo)
 
+if (playerOne['piece'] == 'X'):
+    turn = playerOne
+else:
+    turn = playerTwo
+
 # Initialize empty 2-Dimensional Array
-for x in range(width):
+for x in range(WIDTH):
 
     row = []
 
-    for y in range(height):
+    for y in range(HEIGHT):
         row.append('_')
 
     board.append(row)
 
 # Game loop
-#test comment
 while True:
 
-    printGrid(board, width, height);
+    printGrid(board, WIDTH, HEIGHT);
 
-    nextMove = input("Player " + playerOne['name'] + ": Next move?")
+    setCoordinate(turn)
+
+    coord = board[x][y]
+
+    while (coord != '_'):
+        print("There's a piece already there!")
+        setCoordinate(turn)
+        coord = board[x][y]
+
+    board[x][y] = turn['piece']
+
+    if (turn == playerOne):
+        turn = playerTwo
+    else:
+        turn = playerOne
